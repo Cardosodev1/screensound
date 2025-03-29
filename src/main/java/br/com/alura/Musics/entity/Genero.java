a01e996d78949e6790e793fce06630c5fa4b804a
Guilherme Cardoso <gcs200206@gmail.com>
2025-03-29 20:31:05 -0300

    feat: create Genero and TipoArtista

---
+++ src/main/java/br/com/alura/Musics/entity/Genero.java
@@ -0,0 +1,8 @@
+package br.com.alura.Musics.entity;
+
+public enum Genero {
+    FUNK,
+    TRAP,
+    PAGODE,
+    INTERNACIONAL
+}
---
+++ src/main/java/br/com/alura/Musics/entity/TipoArtista.java
@@ -0,0 +1,7 @@
+package br.com.alura.Musics.entity;
+
+public enum TipoArtista {
+    SOLO,
+    DUPLA,
+    BANDA
+}
